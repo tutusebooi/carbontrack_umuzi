ec13d91e627ae7fc423445e39aaca26cb04b7b90
xolela booi <xbooi023@student.wethinkcode.co.za>
2026-04-09 10:03:14 +0200

    server side

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/eco");
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(3000, () => console.log("Server started"));
