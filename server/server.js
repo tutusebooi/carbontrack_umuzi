2643f19e0a4eac295d91e65f1d40e6e4cb43a2ab
xolela booi <xbooi023@student.wethinkcode.co.za>
2026-04-09 10:33:08 +0200

    server updated

--- server/server.js
+++ server/server.js
@@ -6,10 +6,43 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/eco");
+mongoose.connect("mongodb://127.0.0.1:27017/eco")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Server running");
+const User = require("./models/User");
+
+// REGISTER
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = new User({ email, password, activities: [] });
+  await user.save();
+
+  res.json({ message: "User registered" });
+});
+
+// LOGIN
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email, password });
+
+  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+  res.json(user);
+});
+
+// SAVE ACTIVITY
+app.post("/activity", async (req, res) => {
+  const { email, activity } = req.body;
+
+  const user = await User.findOne({ email });
+  user.activities.push(activity);
+
+  await user.save();
+
+  res.json(user);
 });
 
 app.listen(3000, () => console.log("Server started"));
